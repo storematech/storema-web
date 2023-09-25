803339191f50909d3f331779bcbcf4a4f4cba4a3
Ree-m <reembsrat@gmail.com>
2023-09-25 18:19:47 +0300

    fetched basic data from firebase

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,48 @@
+"use client"
+import { useState, useEffect } from "react"
+import {
+  collection,
+  addDoc,
+  getDoc,
+  QuerySnapshot,
+  query,
+  onSnapshot,
+  deleteDoc,
+  doc
+} from "firebase/firestore";
+import { db } from "@/config/config";
+interface BusinessInterface{
+  business_name:string,
+  business_type:string
+}
 
 export default function Home() {
+  const [business, setBusiness] = useState<BusinessInterface[]>([])
+
+  useEffect(() => {
+    const q = query(collection(db, "_6006413187_business"));
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      let businessArr:any = [];
+      querySnapshot.forEach((doc) => {
+        console.log("doc", doc)
+        businessArr.push({ ...doc.data(), id: doc.id });
+      });
+      setBusiness(businessArr);
+      console.log("business data", businessArr)
+      return () => unsubscribe()
+    });
+  }, []);
+
   return (
     <main>
-     home page
+      home page
+      {business.length > 0 && (
+      <div>
+        <h1>{business[0].business_name}</h1>
+        <p>{business[0].business_type}</p>
+      </div>
+    )}
     </main>
+
   )
 }
--- config/config.ts
+++ config/config.ts
@@ -1,5 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp,getApps } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -16,6 +18,6 @@ measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
-let firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+export const firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
 
-export default firebase_app;
+export const db = getFirestore(firebase_app)
