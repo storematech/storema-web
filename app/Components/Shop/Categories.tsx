34e1acb7cee9434e3704ba2b5574d8aec004eaba
Ree-m <reembsrat@gmail.com>
2023-09-26 16:10:37 +0300

    items of store filtered by category

--- app/Components/Shop/Categories.tsx
+++ app/Components/Shop/Categories.tsx
@@ -5,38 +5,53 @@ import {
     collection,
     query,
     onSnapshot,
+    where,
 
 } from "firebase/firestore";
 import { db } from "@/config/config";
 
 interface ItemInterface {
+    id: string,
     item_name: string,
     item_category_text: string
 }
 export default function Categories({ params }: { params: { business_number: string | null } }) {
-    // for now, display all item of one store
 
-    const [items, setItems] = useState<ItemInterface[]>([])
+    const [itemsOfCategory, setItemsOfCategory] = useState<{ [key: string]: ItemInterface[] }>({})
+
     useEffect(() => {
+        // map to store items of a category
+        const itemsOfCategoryMap: { [key: string]: ItemInterface[] } = {}
+
         const collectionName = `_${params.business_number}_items`;
 
         const q = query(collection(db, collectionName));
         const unsubscribe = onSnapshot(q, (querySnapshot) => {
-            let itemsArr: any = [];
             querySnapshot.forEach((doc) => {
-                itemsArr.push({ ...doc.data(), id: doc.id });
+                const itemData = { ...doc.data(), id: doc.id };
+                const category = itemData.item_category_text;
+                console.log("cateogry", category, itemData)
+                if (!itemsOfCategoryMap[category]) {
+                    itemsOfCategoryMap[category] = []
+                }
+                itemsOfCategoryMap[category].push(itemData)
             });
-            setItems(itemsArr);
-            console.log("items data", itemsArr)
+            setItemsOfCategory(itemsOfCategoryMap)
+            console.log("itemsOfCategoryMap", itemsOfCategoryMap, itemsOfCategory)
             return () => unsubscribe()
         });
     }, [params.business_number]);
     return (
         <div>
-            <h1>All Items</h1>
-            {items.map((item: ItemInterface, index: number) => (
+            {Object.keys(itemsOfCategory).map((category: string, index: number) => (
                 <div key={index}>
-                    <h3>{item.item_name}</h3>
+                    <h2 className="text-3xl">{category}</h2>
+                    {itemsOfCategory[category].map((item:ItemInterface, index) => (
+                        <div key={index}>
+                            <h3>{item.item_name}</h3>
+                        </div>
+                    ))}
+
                 </div>
             ))}
 
